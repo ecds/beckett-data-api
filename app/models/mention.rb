8a17a9735794ee9f4bc65bfcdd1d7087daf9804c
Jay Varner <jayvarner@gmail.com>
2022-07-07 07:29:39 -0400

    Add type to mentions

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -3,4 +3,13 @@
 class Mention < ApplicationRecord
   belongs_to :entity
   belongs_to :letter
+
+  Entity.e_types.each_key do |type|
+    scope type.to_sym, lambda {
+      joins(:entity)
+        .where(
+          'entities.e_type': Entity.e_types[type.to_sym]
+        )
+    }
+  end
 end
