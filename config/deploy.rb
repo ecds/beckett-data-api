5dc0ac35abd9bc73225b7be7def712be7da7fedf
Jay Varner <jayvarner@gmail.com>
2022-08-12 10:52:03 -0400

    Fix deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,8 +3,8 @@
 # config valid for current version and patch releases of Capistrano
 lock '~> 3.17.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'beckett-data-api'
+set :repo_url, 'git@github.com:ecds/beckett-data-api.git'
 
 append :linked_files, 'config/master.key'
 
