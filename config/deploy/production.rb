117a7788a90361d55b6956669110d53950d24de3
Jay Varner <jayvarner@gmail.com>
2023-08-14 10:29:28 -0400

    Update prod deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,9 +3,9 @@
 set :branch, 'develop'
 
 # server '44.192.30.237', user: 'deploy', roles: %w{app db web}, primary: :my_value
-role :app, %w[3.238.231.26], user: 'deploy'
-role :web, %w[3.238.231.26], user: 'deploy'
-role :db,  %w[3.238.231.26], user: 'deploy'
+role :app, %w[44.192.114.189], user: 'deploy'
+role :web, %w[44.192.114.189], user: 'deploy'
+role :db,  %w[44.192.114.189], user: 'deploy'
 
 set :deploy_to, '/data/beckett'
 
