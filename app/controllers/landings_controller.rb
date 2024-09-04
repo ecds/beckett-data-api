7386245b84d621bb46befa7d39dfb67639c517d2
Jay Varner <jayvarner@gmail.com>
2024-09-04 16:04:10 -0400

    Redirect landing to chercher

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class LandingsController < ApplicationController
-  def index; end
-  # empty
+  def index
+    redirect_to 'https://chercherbeckettletters.emory.edu/', status: :moved_permanently, allow_other_host: true
+  end
 end
